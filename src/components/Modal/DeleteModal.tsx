cc849eecd6ab9ec36557349ecceda5bc21a5f3e1
jingjing2222 <131797378+jingjing2222@users.noreply.github.com>
2025-01-22 09:20:39 +0900

    [독후감 사이트 #18] Delete 버튼 구현 완료, 삭제 미구현

--- src/components/Modal/DeleteModal.tsx
+++ src/components/Modal/DeleteModal.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { Link } from "react-router";
 
 export default function DeleteModal({
     closeModal,
@@ -12,10 +13,19 @@ export default function DeleteModal({
                     삭제하시겠습니까?
                 </div>
                 <div className="flex justify-center">
-                    <Button className="flex-initial bg-[#B22222] hover:bg-[#CD5C5C] mr-3">
-                        삭제
-                    </Button>
-                    <Button onClick={closeModal} className="flex-initial ml-3">
+                    <Link to="/">
+                        <Button
+                            type="submit"
+                            className="flex-initial bg-[#B22222] hover:bg-[#CD5C5C] mr-3"
+                        >
+                            삭제
+                        </Button>
+                    </Link>
+                    <Button
+                        type="button"
+                        onClick={closeModal}
+                        className="flex-initial ml-3"
+                    >
                         아니요
                     </Button>
                 </div>
