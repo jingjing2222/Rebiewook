aa7794e6e852ececba919e8949aede0822d19c12
jingjing2222 <131797378+jingjing2222@users.noreply.github.com>
2025-01-22 10:58:38 +0900

    [독후감 사이트 #21] 메인창 Loading 기능 추가, UI 점검 필요

--- src/components/BookReviewGrid.tsx
+++ src/components/BookReviewGrid.tsx
@@ -15,9 +15,10 @@ interface Review {
 
 export const BookReviewGrid = () => {
     const [reviews, setReviews] = useState<Review[]>([]);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
-        (async () => {
+        async function fetchReviews() {
             const { data: book, error } = await supabase
                 .from("book")
                 .select("*");
@@ -28,10 +29,13 @@ export const BookReviewGrid = () => {
             }
 
             const camelCasedBook = camelcaseKeys(book);
+            setLoading(false);
             setReviews(camelCasedBook);
-        })();
+        }
+        setLoading(true);
+        fetchReviews();
     }, []);
-
+    if (loading) return <div>loading...</div>;
     return (
         <div className="bookshelf">
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
