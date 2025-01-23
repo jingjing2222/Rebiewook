9c689ee6666b13f2445c897d832309b467e6b399
jingjing2222 <131797378+jingjing2222@users.noreply.github.com>
2025-01-23 17:06:32 +0900

    [독후감 사이트 #40] 모달 개인화 완료, 안에 폼 어떻게 넣을지 고민

--- src/components/Modal/ModalContext.tsx
+++ src/components/Modal/ModalContext.tsx
@@ -1,25 +1,69 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer, useState } from "react";
 import {
     Dialog,
     DialogContent,
     DialogHeader,
     DialogTitle,
-    DialogTrigger,
 } from "@/components/ui/dialog";
 import { createPortal } from "react-dom";
 
 interface ModalContextProps {
     openModal: () => void;
     closeModal: () => void;
+    dispatch: (action: ActionType) => void;
+}
+
+interface ActionType {
+    type: string;
+}
+
+interface ModalType {
+    title: string;
+    content: string;
+    what: string;
 }
 
 export const ModalContext = createContext<ModalContextProps>({
     openModal: () => {},
     closeModal: () => {},
+    dispatch: () => {},
 });
+
+function reducer(modalType: ModalType, action: ActionType) {
+    switch (action.type.toUpperCase()) {
+        case "LOGIN":
+            return {
+                ...modalType,
+                title: "Login",
+                content: "Login 내용",
+                what: "LOGIN",
+            };
+        case "DELETE":
+            return {
+                ...modalType,
+                title: "Delete",
+                content: "Delete 내용",
+                what: "DELETE",
+            };
+        case "DEFAULT":
+        default:
+            return {
+                ...modalType,
+                title: "Default",
+                content: "Default 내용",
+                what: "DEFAULT",
+            };
+    }
+}
 
 export function ModalProvider({ children }: { children: React.ReactNode }) {
     const [isOpen, setIsOpen] = useState(false);
+    const Type = {
+        title: "Default",
+        content: "Default 내용",
+        what: "DEFAULT",
+    };
+    const [modalType, dispatch] = useReducer(reducer, Type);
 
     function openModal() {
         setIsOpen(true);
@@ -30,17 +74,17 @@ export function ModalProvider({ children }: { children: React.ReactNode }) {
     };
 
     return (
-        <ModalContext.Provider value={{ openModal, closeModal }}>
+        <ModalContext.Provider value={{ openModal, closeModal, dispatch }}>
             {children}
             {createPortal(
                 <Dialog open={isOpen} onOpenChange={setIsOpen}>
-                    <DialogTrigger asChild></DialogTrigger>
                     <DialogContent className="sm:max-w-[425px] bg-[#F4A460] bg-opacity-90">
                         <DialogHeader>
                             <DialogTitle className="text-2xl font-bold text-[#8B4513]">
-                                {"모달"}
+                                {modalType.title}
                             </DialogTitle>
                         </DialogHeader>
+                        {modalType.content}
                     </DialogContent>
                 </Dialog>,
                 document.body
--- src/routes/pages/Home.tsx
+++ src/routes/pages/Home.tsx
@@ -1,14 +1,20 @@
 import { BookReviewGrid } from "@/components/BookReviewGrid";
 import { ModalContext } from "@/components/Modal/ModalContext";
-
 import { useContext } from "react";
 
 export default function Home() {
-    const { openModal, closeModal } = useContext(ModalContext);
+    const { openModal, dispatch } = useContext(ModalContext);
     return (
         <>
             <div className="container mx-auto px-4 py-8">
-                <button onClick={() => openModal()}>모달 출력!</button>
+                <button
+                    onClick={() => {
+                        openModal();
+                        dispatch({ type: "delete" });
+                    }}
+                >
+                    모달 출력!
+                </button>
                 <h1 className="text-3xl font-bold mb-8 text-center text-[#4B3621] shadow-text">
                     최근 독후감
                 </h1>
