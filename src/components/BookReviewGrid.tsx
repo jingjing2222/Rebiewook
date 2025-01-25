5b7263cec38c10841b60d9585998c9d02e8c2653
jingjing2222 <131797378+jingjing2222@users.noreply.github.com>
2025-01-25 09:53:41 +0900

    [독후감 사이트 #48] Refactor: 메인 화면 useEffect => useQuery 변경

--- src/components/BookReviewGrid.tsx
+++ src/components/BookReviewGrid.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from "react";
 import { BookCard } from "./BookCard";
 import { supabase } from "@/supabase/Client";
 import camelcaseKeys from "camelcase-keys";
+import { useQuery } from "@tanstack/react-query";
 
 interface Review {
     author: string;
@@ -14,10 +14,7 @@ interface Review {
 }
 
 export const BookReviewGrid = () => {
-    const [reviews, setReviews] = useState<Review[]>([]);
-    const [loading, setLoading] = useState(false);
-
-    async function fetchReviews() {
+    const fetchReviews = async () => {
         const { data: book, error } = await supabase
             .from("book")
             .select("*")
@@ -27,24 +24,32 @@ export const BookReviewGrid = () => {
             console.error(error);
             return;
         }
+        return book;
+    };
 
-        const camelCasedBook = camelcaseKeys(book);
-        setLoading(false);
-        setReviews(camelCasedBook);
+    const { data, isLoading, isError } = useQuery({
+        queryKey: ["fetchReviews"],
+        queryFn: fetchReviews,
+        staleTime: 1000 * 10,
+    });
+
+    if (isLoading) {
+        return <div>loading...</div>;
     }
-
-    useEffect(() => {
-        setLoading(true);
-        fetchReviews();
-    }, []);
-    if (loading) return <div>loading...</div>;
-    return (
-        <div className="bookshelf">
-            <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
-                {reviews.map((review: Review) => (
-                    <BookCard key={review.id} review={review} />
-                ))}
-            </div>
-        </div>
-    );
+    if (isError) {
+        return <div>Error fetching data</div>;
+    }
+    if (data) {
+        return (
+            <>
+                <div className="bookshelf">
+                    <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
+                        {camelcaseKeys(data).map((review: Review) => (
+                            <BookCard key={review.id} review={review} />
+                        ))}
+                    </div>
+                </div>
+            </>
+        );
+    }
 };
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,16 @@ import { createRoot } from "react-dom/client";
 import Router from "@/routes/Router";
 import "@/main.css";
 import { CookiesProvider } from "react-cookie";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <CookiesProvider>
+        <QueryClientProvider client={queryClient}>
+            <CookiesProvider>
                 <Router />
-        </CookiesProvider>
+            </CookiesProvider>
+        </QueryClientProvider>
     </StrictMode>
 );
