1bf8c131bb70ad3946fc89630f31fd2e044f8e21
jingjing2222 <131797378+jingjing2222@users.noreply.github.com>
2025-02-03 19:45:02 +0900

    [독후감 사이트 #83] Feat: 모바일 Home 애니메이션 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,64 +1,85 @@
-import { BookCard } from "@/pages/home/BookCard";
-import { supabase } from "@/supabase/Client";
-import { useQuery } from "@tanstack/react-query";
-import camelcaseKeys from "camelcase-keys";
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router";
 import { motion } from "framer-motion";
 
-interface Review {
-    author: string;
-    coverImage: string;
-    description: string;
-    detailedReview: string;
-    id: number;
-    publishedDate: string;
-    title: string;
-}
-
-const fetchReviews = async () => {
-    const { data: book } = await supabase
-        .from("book")
-        .select("*")
-        .order("published_date", { ascending: false });
-
-    return book;
-};
-
-export default function Home() {
-    const { status, data } = useQuery({
-        queryKey: ["fetchReviews"],
-        queryFn: fetchReviews,
-        staleTime: 1000 * 60,
-    });
+export const Home = () => {
+    const navigate = useNavigate();
     return (
-        <>
-            <div className="container mx-auto px-4 py-4 md:py-6">
-                <motion.div
-                    initial={{ opacity: 0, y: 20 }}
-                    whileInView={{
-                        opacity: 1,
-                        y: 0,
-                        transition: { delay: 0.1 },
-                    }}
-                    className="text-xl md:text-2xl font-bold mb-4 text-center text-[#4B3621] shadow-text"
+        <div className="flex flex-col items-center mt-40">
+            <motion.div
+                initial={{ y: -50, opacity: 0 }}
+                animate={{
+                    y: 0,
+                    opacity: 100,
+                    transition: { duration: 1, delay: 0.3 },
+                }}
+                className="flex-1 text-5xl mb-3"
+            >
+                안녕하세요
+            </motion.div>
+            <motion.div
+                initial={{ x: 50, opacity: 0 }}
+                animate={{
+                    x: 0,
+                    opacity: 100,
+                    transition: { duration: 1, delay: 1.3 },
+                }}
+                className="flex-1 text-3xl"
+            >
+                오신 것을 환영합니다!
+            </motion.div>
+            <br />
+            <motion.div
+                className="text-md text-stone-900 my-2"
+                initial={{ x: -600, opacity: 0 }}
+                animate={{
+                    x: 0,
+                    opacity: 100,
+                    transition: { duration: 1, delay: 2.3 },
+                }}
+            >
+                1. 인문, 세계문학전집 위주로 읽습니다.
+            </motion.div>
+            <motion.div
+                className="text-md text-stone-900 my-2"
+                initial={{ x: -600, opacity: 0 }}
+                animate={{
+                    x: 0,
+                    opacity: 100,
+                    transition: { duration: 1, delay: 2.8 },
+                }}
+            >
+                2. 짧거나 성의가 안보이면 흥미가 없는 책일 수도..
+            </motion.div>
+            <motion.div
+                className="text-md text-red-600 my-2"
+                initial={{ opacity: 0 }}
+                animate={{
+                    opacity: 100,
+                    transition: { duration: 1, delay: 3.8 },
+                }}
+            >
+                ※ 스포일러가 포함될 수 있습니다.
+            </motion.div>
+            <motion.div
+                className="flex items-center mt-2"
+                initial={{ y: -5 }}
+                animate={{
+                    y: 0,
+                    transition: {
+                        duration: 1,
+                        repeat: Infinity,
+                        repeatType: "reverse",
+                    },
+                }}
+            >
+                <Button
+                    className="flex-1"
+                    onClick={() => navigate("/reviewlistpage")}
                 >
-                    최근 독후감
-                </motion.div>
-                {status === "pending" ? (
-                    <div>loading...</div>
-                ) : status === "error" ? (
-                    <div>Error fetching data</div>
-                ) : (
-                    <>
-                        <div className="bookshelf">
-                            <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
-                                {camelcaseKeys(data!).map((review: Review) => (
-                                    <BookCard key={review.id} review={review} />
-                                ))}
-                            </div>
-                        </div>
-                    </>
-                )}
-            </div>
-        </>
+                    살펴보기
+                </Button>
+            </motion.div>
+        </div>
     );
-}
+};
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,7 +1,9 @@
 import Layout from "@/layout/Layout";
 import BookDetailesPage from "@/pages/BookDetailesPage";
 import { EditPage } from "@/pages/EditPage";
-import Home from "@/pages/Home";
+import { Home } from "@/pages/Home";
+import ReviewListPage from "@/pages/ReviewListPage";
+
 import { UploadPage } from "@/pages/UploadPage";
 import { createBrowserRouter, RouterProvider } from "react-router";
 
@@ -15,15 +17,19 @@ const router = createBrowserRouter([
                 element: <Home />,
             },
             {
-                path: "bookdetailes/:id",
+                path: "reviewlistpage",
+                element: <ReviewListPage />,
+            },
+            {
+                path: "/bookdetailes/:id",
                 element: <BookDetailesPage />,
             },
             {
-                path: "upload",
+                path: "/upload",
                 element: <UploadPage />,
             },
             {
-                path: "bookdetailes/:id/edit",
+                path: "/bookdetailes/:id/edit",
                 element: <EditPage />,
             },
         ],
