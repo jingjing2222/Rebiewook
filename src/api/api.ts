5f89f6628201073b358ce500606aa3f436212944
jingjing2222 <gudwjd1208@koreatech.ac.kr>
2025-05-26 22:34:39 +0900

    refact: delete anon key

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,9 +1,6 @@
 const API_BASE_URL =
   "https://tstnssgozphknyeryvtq.supabase.co/functions/v1/api";
-const ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRzdG5zc2dvenBoa255ZXJ5dnRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzczNDk4MTgsImV4cCI6MjA1MjkyNTgxOH0.ETH7zFCQD-LxiV_9UcyVX4eHgjbv8z3kXgMs1pJ20j0";
 const headers = {
-  apikey: ANON_KEY,
   "Content-Type": "application/json",
 };
 
