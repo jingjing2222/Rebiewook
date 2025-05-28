fb6849c8a8a59bc5937fa33169528f4bf1f5a788
jingjing2222 <gudwjd1208@koreatech.ac.kr>
2025-05-28 10:05:20 +0900

    fix: type

--- src/pages/UploadPage.tsx
+++ src/pages/UploadPage.tsx
@@ -1,6 +1,7 @@
 import UploadBasedForm from "@/pages/UploadForm/UploadBasedForm";
 import { supabase } from "@/supabase/Client";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 
 declare global {
@@ -20,6 +21,14 @@ export interface DBBook {
   title: string;
 }
 
+export interface IselectedImage {
+  base64: string;
+  width: string;
+  height: string;
+  fileName: string;
+  fileSize: string;
+}
+
 export const UploadPage = () => {
   const navigate = useNavigate();
   const queryClient = useQueryClient();
@@ -32,6 +41,29 @@ export const UploadPage = () => {
       navigate("/reviewlistpage");
     },
   });
+
+  const [selectedImage, setSelectedImage] = useState<IselectedImage | null>(
+    null,
+  );
+
+  const handleMessage = async (event: MessageEvent) => {
+    try {
+      const messageData = JSON.parse(event.data);
+      if (messageData.type === "imageSelected") {
+        const imageInfo = messageData.data;
+        setSelectedImage(imageInfo);
+      }
+    } catch (error) {
+      console.error("Error parsing message:", error);
+    }
+  };
+
+  useEffect(() => {
+    window.ReactNativeWebView.postMessage("photo");
+    return () => {
+      window.removeEventListener("message", handleMessage);
+    };
+  }, []);
 
   async function insertBook(book: DBBook) {
     const { data, error } = await supabase.from("book").insert(book).select();
@@ -47,9 +79,21 @@ export const UploadPage = () => {
 
   return (
     <>
-      <button className="w-[300px] h-[200px]" onClick={sendMessage}>
-        이 버튼은 웹입니다.
-      </button>
+      <div>
+        <button
+          className="w-[100px] h-[100px] border-2 border-black"
+          onClick={sendMessage}
+        >
+          이 버튼은 웹입니다.
+        </button>
+        {selectedImage && (
+          <img
+            src={selectedImage.base64}
+            alt={selectedImage.fileName}
+            style={{ maxWidth: "100%", height: "auto" }}
+          />
+        )}
+      </div>
       <UploadBasedForm
         onClick={insertBookMutation}
         content="Upload"
